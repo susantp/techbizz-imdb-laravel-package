2addad944e67850e1b7975ad7ad8574132e5426b
Susant Paudel <techbizznepal@gmail.com>
2021-10-21 00:47:20 +0545

    search movie api

--- src/ImdbReader.php
+++ src/ImdbReader.php
@@ -3,6 +3,7 @@
 namespace Techbizz\ImdbReader;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ImdbReader
 {
@@ -21,9 +22,10 @@ class ImdbReader
         $lang = "en";
         $params = "/en/API/SearchMovie/" . $this->apiKey . "/" . $expression;
         try {
+//            return Http::acceptJson()->get($this->endpoint . $params);
             return json_decode(file_get_contents($this->endpoint . $params), true);
         } catch (\Exception $exception) {
-            return $expression->getMessage();
+            return abort(400, $exception->getMessage());
         }
         https://imdb-api.com/en/API/SearchMovie/k_1l3a2ym5/inception 2010
     }
